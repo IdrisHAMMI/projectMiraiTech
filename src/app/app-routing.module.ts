34cad0f1f387bbf0912de52232897a2bff6e9178
IdrisHAMMI <hammi.idris@gmail.com>
2023-07-27 19:34:00 +0200

    added front end base for the login/signup pages. added routing for said pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from './components/index/index.component';
+import { AuthLoginComponent } from './components/auth-login/auth-login.component';
+import { AuthSignupComponent } from './components/auth-signup/auth-signup.component';
+
 
 const routes: Routes = [
   { path: 'index', component: IndexComponent},
+  { path: 'authLogin', component: AuthLoginComponent},
+  { path: 'authSignup', component: AuthSignupComponent},
   { path: '', redirectTo: 'index', pathMatch: 'full'}
 ];
 
